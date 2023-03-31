ff610e7abaacfbaa45ba67714bd8c21c0e1954d6
Polalf <103451330+Polalf@users.noreply.github.com>
2023-03-31 15:05:51 -0300

    a

---
+++ Assets/Scripts/Puzzles.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Puzzles : MonoBehaviour
+{
+    public GameObject door;
+    bool solve;
+    void Update()
+    {
+        if(solve)
+        {
+            door
+        }
+    }
+}
