d853a3fc445007011a7ae2a9d1e416a8977cb9da
Polalf <103451330+Polalf@users.noreply.github.com>
2023-04-26 05:44:38 -0400

    Creación de cueva

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float jumpForce;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float fallMulti;
 
     public Transform groundCheck;
     public LayerMask groundLayer;
     bool isGrounded;
-
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,10 +20,23 @@ public class Jump : MonoBehaviour
    
     void Update()
     {
-        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(2.3f,0.5f),CapsuleDirection2D.Horizontal,0,groundLayer);
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(2.3f, 0.5f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale += fallMulti * Time.deltaTime;
+        }
+        else
+        {
+            rb.gravityScale = 1;
         }
     }
 }
--- Assets/Scripts/Puzzles.cs
+++ Assets/Scripts/Puzzles.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Puzzles : MonoBehaviour
 {
     public GameObject door;
     public bool solve;
+    public UnityEvent resuelto;
     void Update()
     {
-        if(solve)
+        //if (solve)
+        //{
+        //    door.GetComponent<Doors>().openDoor();
+        //}
+        if (solve)
         {
-            door.GetComponent<Doors>().openDoor();
+            resuelto.Invoke();
         }
     }
 }
